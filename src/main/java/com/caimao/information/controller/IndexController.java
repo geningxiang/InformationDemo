dba6b6dbbe4200c6b4a13b345dda9071e4d738ee
genx <geningxiang@qq.com>
2018-12-13 17:33:14 +0800

    配置 springmvc

---
+++ src/main/java/com/caimao/information/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.caimao.information.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: genx
+ * @date: 2018/12/13 17:25
+ */
+@Controller
+
+public class IndexController {
+}
